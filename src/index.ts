9e5ff0c7d72b6e359ccfa3858cd890915f308321
Roberto Salinas <roberto.salinasaguilar@mercadolibre.com>
2025-04-26 15:09:12 -0300

    feat: db dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import pool from "./db";
 
 const app = express();
 app.use(express.json());
 
 app.get("/", (_, res) => {
 	res.send("API Running");
+});
+
+app.get("/db-test", async (_, res) => {
+	try {
+		const result = await pool.query("SELECT NOW()");
+		res.json(result.rows[0]);
+	} catch (err) {
+		console.error(err);
+		res.status(500).json({ error: "Database connection failed" });
+	}
 });
 
 const PORT = process.env.PORT || 3000;
